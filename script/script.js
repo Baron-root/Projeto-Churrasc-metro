adfd1d2fa8b252d2e75a565b3e16ae8e54bd0baa
Baron-root <76979496+Baron-root@users.noreply.github.com>
2022-02-28 20:54:37 -0300

    açteração de script

--- script/script.js
+++ script/script.js
@@ -3,3 +3,31 @@
 // Refrigerante/Agua - 1000 ml por Pessoa + 6 Horas 1500 ml
 
 //Criança valem por  0,5
+let inputAdultos = document.getElementById("adultos");
+let inputCrianca = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+  console.log("Calcular");
+
+  let adultos = inputAdultos.value;
+  let criancas = inputCrianca.value;
+  let duracao = inputDuracao.value;
+
+  let qdtTotalCarne =
+    carnePP(duracao) + adultos + (carnePP(duracao) / 2 + criancas);
+  console.log(qdtTotalCarne);
+}
+
+function carnePP(duracao) {
+  let carne = 400;
+  if (duracao >= 6) {
+    carne = 650;
+    return 650;
+  } else {
+    return 400;
+  }
+  return carne;
+}
