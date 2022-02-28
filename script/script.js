b2b55c78ccc75088ed1324b5e09c32a58701e37e
Baron-root <76979496+Baron-root@users.noreply.github.com>
2022-02-28 19:23:05 -0300

    alteração do css

---
+++ script/script.js
@@ -0,0 +1,5 @@
+// Carne  - 400gr por Pessoa  + 6 Horas  - 650gr
+// Cerveja - 1200 ml por Pessoa + 6 Horas - 2000 ml
+// Refrigerante/Agua - 1000 ml por Pessoa + 6 Horas 1500 ml
+
+//Criança valem por  0,5
